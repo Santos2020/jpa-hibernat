5d318712c1a9f83d4f010572aabc7601f4ab9832
carlos santos <carlos.aes.1977@gmail.com>
2023-07-17 10:30:30 -0300

    criando orm xml query

--- src/main/java/com/restaurante/food/api/controller/TesteController.java
+++ src/main/java/com/restaurante/food/api/controller/TesteController.java
@@ -1,31 +1,38 @@
 package com.restaurante.food.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restaurante.food.domain.model.Cozinha;
 import com.restaurante.food.domain.repository.CozinhaRepository;
+import com.restaurante.food.domain.repository.RestauranteRepository;
 
 @RestController
 @RequestMapping("/teste")
 public class TesteController {
 
-	/*
-	 * @Autowired private CozinhaRepository cozinhaRepository;
-	 */
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 	
-//	@GetMapping("/cozinhas/por-nome")
-//	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
-//		return cozinhaRepository.consultarPorNome(nome);
-//		
-//	}
+
+	@GetMapping("/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNome(String nome) {
+		return cozinhaRepository.findTodasByNomeContaining(nome);
+	}
+
+	@GetMapping("/cozinhas/unica-por-nome")
+	public Optional<Cozinha> cozinhaPorNome(String nome) {
+		return cozinhaRepository.findByNome(nome);
+	}
+
+	@GetMapping("/cozinhas/exists")
+	public boolean cozinhaExists(String nome) {
+		return cozinhaRepository.existsByNome(nome);
+	}
 
 }
-
-
-
